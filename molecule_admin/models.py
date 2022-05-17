74a0860fb3366824b1bb43f5b07ec1724d6ef976
Cathleen <grohmannova.k@seznam.cz>
2022-05-17 15:34:59 +0200

    Ahoj, další pokus o Commit and Push :D

--- molecule_admin/models.py
+++ molecule_admin/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+#from django.contrib.auth.models import User
+#from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Collection(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+
+
+class Molecule(models.Model):
+    name = models.CharField(max_length=200)
+    smiles = models.CharField(max_length=500)
+    inchi_key = models.CharField(max_length=100)
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField()
+
+
+class User(models.Model):
+    primary_company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+
+
