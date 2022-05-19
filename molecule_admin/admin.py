119b254126fcd8be055c498cd9e02947a9f7bf2f
szeta2 <szeteiova.p@gmail.com>
2022-05-19 23:04:12 +0200

    changes in index.html, english language, registration of models in admin.py

--- molecule_admin/admin.py
+++ molecule_admin/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+import molecule_admin.models as models
+# Create your models here.
+class MoleculeAdmin(admin.ModelAdmin):
+    fields = ["name", "smiles", "inchi_key", "collection", "image"]
+    readonly_fields = ["inchi_key"]
+    list_display = ["name", "smiles", "inchi_key", "collection", "image"]
+    list_filter = ["name", "smiles", "inchi_key", "collection"]
+    search_fields = ["name", "collection"]
 
-# Register your models here.
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+
+admin.site.register(models.Molecule, MoleculeAdmin)
+admin.site.register(models.Collection, CollectionAdmin)
+
--- molecule_admin/models.py
+++ molecule_admin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Collection(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+
+class Molecule(models.Model):
+    name = models.CharField(max_length=200)
+    smiles = models.CharField(max_length=500)
+    inchi_key = models.CharField(max_length=100)
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField()
