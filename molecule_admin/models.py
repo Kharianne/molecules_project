806ba567233edb6569610733b52b2aab31acea63
szeta2 <szeteiova.p@gmail.com>
2022-05-20 21:47:57 +0200

    revert changes on this file, not finished with it yet

--- molecule_admin/models.py
+++ molecule_admin/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-class Collection(models.Model):
-    name = models.CharField(max_length=200, blank=True, null=True)
-
-class Molecule(models.Model):
-    name = models.CharField(max_length=200)
-    smiles = models.CharField(max_length=500)
-    inchi_key = models.CharField(max_length=100)
-    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
-    image = models.ImageField()
+# Create your models here.
