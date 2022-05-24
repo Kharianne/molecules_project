c66002346eef7887ffbc955b153bb9325b69eedf
Cathleeen <grohmannova.k@seznam.cz>
2022-05-24 14:25:55 +0200

    .

--- molecule_admin/models.py
+++ molecule_admin/models.py
@@ -1,29 +1,33 @@
 from django.db import models
 #from django.contrib.auth.models import User
 #from django.db.models.signals import post_save
+from django.db.models.constraints import UniqueConstraint
 
 
-class Collection(models.Model):
-    name = models.CharField(max_length=200, blank=True, null=True)
+class Collection(models.Model, primary_key=True):
+    name = models.CharField(max_length=200, blank=True, null=True, primary_key=True, unique=True)
 
 
 class Molecule(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, primary_key=True, unique=True)
     smiles = models.CharField(max_length=500)
     inchi_key = models.CharField(max_length=100)
-    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, unique=True)
     image = models.ImageField()
 
-class Company(models.Model):
-
-    name = models.CharField(max_length=50)
-    identification_number = models.CharField(max_length=100)
-
     class Meta:
-        verbose_name_plural = 'Companies'
+        UniqueConstraint(fields=['Collection', 'Molecule'], name='unique_collection')
 
 class User(models.Model):
-    primary_company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    COMPANY_CHOICES = (
+        ('a', 'GREEN'),
+        ('b', 'BLUE'),
+        ('c', 'RED'),
+        ('d', 'ORANGE'),
+        ('e', 'BLACK'),
+    )
+
+    company = models.CharField(max_length=6, choices=COMPANY_CHOICES, default='a', on_delete=models.SET_NULL, null=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
