3bed289cffd1901a0a11fa11361779b7d498b76c
kharianne <katerinamrkackova@gmail.com>
2022-06-03 21:26:54 +0200

    Improve admin view

--- molecule_admin/admin.py
+++ molecule_admin/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-import molecule_admin.models as models
-# Create your models here.
+from django.contrib import admin
+from django.db import models
+from django.forms import CheckboxSelectMultiple
+
+from molecule_admin.models import Molecule, Collection
+
+
 class MoleculeAdmin(admin.ModelAdmin):
     fields = ["name", "smiles", "inchi_key", "collection", "image"]
-    readonly_fields = ["inchi_key"]
-    list_display = ["name", "smiles", "inchi_key", "collection", "image"]
-    list_filter = ["name", "smiles", "inchi_key", "collection"]
+    list_display = ["name", "inchi_key", "collections"]
+    list_filter = ["collection"]
     search_fields = ["name", "collection"]
+
+    # Changes multiple select to CheckboxSelectMultiple
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
+
+    # Disables adding new collection via MoleculeAdmin
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['collection'].widget.can_change_related = False
+        form.base_fields['collection'].widget.can_add_related = False
+        return form
+
+    # Allows to add collections to list_display
+    def collections(self, obj):
+        return ", ".join([p.name for p in obj.collection.all()])
+
 
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ["name"]
@@ -14,7 +35,6 @@ class CollectionAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 
-
-admin.site.register(models.Molecule, MoleculeAdmin)
-admin.site.register(models.Collection, CollectionAdmin)
+admin.site.register(Molecule, MoleculeAdmin)
+admin.site.register(Collection, CollectionAdmin)
 
