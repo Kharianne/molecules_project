87f561e8de7fff1dd1f2a4a62890a05ab46cc143
kharianne <katerinamrkackova@gmail.com>
2022-06-03 18:21:16 +0200

    Adjustments to models
    - change of Constraint for Molecules - reference was made to Model not
      to field
    - change ForeignKey to ManyToMany in Molecule_collection to have all connections to Collections
    - deleteion of User model - will be implemented in next iteration
    - new migration file
    - detele contents of README.md

--- molecule_admin/models.py
+++ molecule_admin/models.py
@@ -1,34 +1,37 @@
 from django.db import models
-#from django.contrib.auth.models import User
-#from django.db.models.signals import post_save
 from django.db.models.constraints import UniqueConstraint
 
 
-class Collection(models.Model, primary_key=True):
-    name = models.CharField(max_length=200, blank=True, null=True, primary_key=True, unique=True)
+class Collection(models.Model):
+    name = models.CharField(max_length=200,
+                            primary_key=True,
+                            unique=True)
 
 
 class Molecule(models.Model):
-    name = models.CharField(max_length=200, primary_key=True, unique=True)
+    name = models.CharField(max_length=200)
     smiles = models.CharField(max_length=500)
-    inchi_key = models.CharField(max_length=100)
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, unique=True)
-    image = models.ImageField()
+    inchi_key = models.CharField(max_length=500)
+    collection = models.ManyToManyField(Collection)
+    image = models.ImageField(blank=True, null=True)
 
+    # Inchi_key is nearly unique, so we need to be sure that we don't have
+    # duplicates
     class Meta:
-        UniqueConstraint(fields=['Collection', 'Molecule'], name='unique_collection')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'inchi_key'], name='unique-molecule-constraint'
+            )
+        ]
 
-class User(models.Model):
-    COMPANY_CHOICES = (
-        ('a', 'GREEN'),
-        ('b', 'BLUE'),
-        ('c', 'RED'),
-        ('d', 'ORANGE'),
-        ('e', 'BLACK'),
-    )
 
-    company = models.CharField(max_length=6, choices=COMPANY_CHOICES, default='a', on_delete=models.SET_NULL, null=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
+"""
+TODO: Create a model named Profile with following fields:
+- user = models.OneToOneField(User, on_delete=models.CASCADE) --> User model
+has to be imported from django.contrib.auth.models
+- institution
+- name
+- surname
+
+Create a post_save signal to create Profile for User after registration
+"""
