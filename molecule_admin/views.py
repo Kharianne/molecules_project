a384c070a66194ef14501f7b9f3961ac8743cc41
kharianne <katerinamrkackova@gmail.com>
2022-06-06 14:09:30 +0200

    Molecule ListView

--- molecule_admin/views.py
+++ molecule_admin/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView
+
+from molecule_admin.models import Molecule, Collection
 
 
-# Create your views here.
 class IndexView(TemplateView):
     template_name = "index.html"
+
+
+class MoleculeListView(LoginRequiredMixin, ListView):
+    paginate_by = 32
+    model = Molecule
+    template_name = "molecules/molecule_list.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['collections'] = Collection.objects.all().order_by('name')
+        search_query = self.request.GET.get('search')
+        context['search'] = search_query
+        print(context)
+        return context
+
+    def get_queryset(self):
+        print(self.request.GET)
+        filter_collection = self.request.GET.getlist('collection')
+        search_query = self.request.GET.get('search')
+        if filter_collection and search_query:
+            return Molecule.objects.filter(collection__in=filter_collection,
+                                           name__icontains=search_query)\
+                .order_by('name')
+        elif filter_collection:
+            return Molecule.objects.filter(collection__in=filter_collection)\
+                .order_by('name')
+        elif search_query:
+            return Molecule.objects.filter(name__icontains=search_query)\
+                .order_by('name')
+        else:
+            return Molecule.objects.all().order_by('name')
--- molecule_project/settings.py
+++ molecule_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "index"
-LOGOUT_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "molecules"
+LOGOUT_REDIRECT_URL = "login"
