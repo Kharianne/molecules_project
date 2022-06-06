19fed73032ce20a3e020db2b4202126820d03151
Kharianne <katerinamrkackova@gmail.com>
2022-06-06 20:24:25 +0200

    Detail of molecule (#8)
    - new DetailView for molecule detail with a template
    - added new property 'collections' to Molecule model to have one place
      where we get a list of all collections for given molecule, deleted the
      same thing from admin, now it is only on model
    - change on MoleculeListView - url to molecule detail

--- molecule_admin/urls.py
+++ molecule_admin/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path("molecules/", views.MoleculeListView.as_view(), name='molecules')
+    path("molecules/", views.MoleculeListView.as_view(), name='molecules'),
+    path("molecule/<int:pk>", views.MoleculeDetailView.as_view(),
+         name='molecule_detail')
 ]
