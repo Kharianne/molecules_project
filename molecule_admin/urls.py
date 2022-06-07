dd950bcd6fb85404975b8e0dbf7014724601fe5d
kharianne <katerinamrkackova@gmail.com>
2022-06-08 01:45:29 +0200

    User notes
    - new model Note that contains notes for given user and given molecule
    - new views to be able create, update and delete a note
    - new templates for views above
    - notes are visible on MoleculeDetailView

--- molecule_admin/urls.py
+++ molecule_admin/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path("molecules/", views.MoleculeListView.as_view(), name='molecules'),
     path("molecule/<int:pk>", views.MoleculeDetailView.as_view(),
-         name='molecule_detail')
+         name='molecule_detail'),
+    path("note/create", views.NoteCreateView.as_view(), name='create_note'),
+    path("note/delete/<int:pk>", views.NoteDeleteView.as_view(),
+         name='delete_note'),
+    path("note/update/<int:pk>", views.NoteUpdateView.as_view(),
+         name='update_note')
 ]
--- molecule_admin/views.py
+++ molecule_admin/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, DetailView
+from django.core.exceptions import PermissionDenied
+from django.urls import reverse
+from django.views.generic import TemplateView, ListView, DetailView, \
+    CreateView, UpdateView, DeleteView
 
-from molecule_admin.models import Molecule, Collection
+from molecule_admin.forms import CreateNoteForm
+from molecule_admin.models import Molecule, Collection, Note
 
 
 class IndexView(TemplateView):
@@ -40,3 +44,62 @@ class MoleculeListView(LoginRequiredMixin, ListView):
 class MoleculeDetailView(LoginRequiredMixin, DetailView):
     model = Molecule
     template_name = "molecules/molecule_detail.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['notes'] = Note.objects.filter(molecule=context.get('molecule').pk,
+                                               user_id=self.request.user.id)
+        return context
+
+
+class NoteCreateView(LoginRequiredMixin, CreateView):
+    model = Note
+    template_name = "notes/create_note.html"
+    form_class = CreateNoteForm
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['molecule'] = self.request.GET.get('molecule')
+        return initial
+
+    def get_form(self, form_class=None):
+        form = super(NoteCreateView, self).get_form(form_class)
+        form.instance.user_id = self.request.user
+        return form
+
+    def get_success_url(self):
+        return reverse('molecule_detail',
+                       kwargs={'pk': self.request.GET.get('molecule')})
+
+
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Note
+    template_name = "notes/delete_note.html"
+
+    def get_success_url(self):
+        return reverse('molecule_detail',
+                       kwargs={'pk': self.request.GET.get('molecule')})
+
+    def dispatch(self, request, *args, **kwargs):
+        note_user = self.get_object().user_id.pk
+        if note_user != self.request.user.id:
+            raise PermissionDenied("It seems like you are trying to delete "
+                                   "a note that does not belong to you.")
+        return super(NoteDeleteView, self).dispatch(request)
+
+
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Note
+    template_name = "notes/update_note.html"
+    fields = ['note_text']
+
+    def get_success_url(self):
+        return reverse('molecule_detail',
+                       kwargs={'pk': self.request.GET.get('molecule')})
+
+    def dispatch(self, request, *args, **kwargs):
+        note_user = self.get_object().user_id.pk
+        if note_user != self.request.user.id:
+            raise PermissionDenied("It seems like you are trying to update "
+                                   "a note that does not belong to you.")
+        return super(NoteUpdateView, self).dispatch(request)
