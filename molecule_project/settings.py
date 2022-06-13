c92c4a935962f928d060268d27bb4baaba904e5c
kharianne <katerinamrkackova@gmail.com>
2022-06-13 19:11:19 +0200

    CSRF trusted origins

--- molecule_project/settings.py
+++ molecule_project/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'molecules.wolfsden.cz']
+CSRF_TRUSTED_ORIGINS = ['https://molecules.wolfsden.cz']
 
 # Application definition
 
